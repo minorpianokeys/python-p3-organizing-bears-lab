969a6d87c42a5c721e72eb0e58df8e42939b9a60
Michael Friedman <minorpianokeys@gmail.com>
2023-12-26 14:43:53 -0500

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Brown", "Chill", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "M", "Grey", "Rowdy", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 2, "M", "Grey", "Rowdy", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 2, "M", "Grey", "Rowdy", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 2, "M", "Grey", "Rowdy", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 2, "M", "Grey", "Rowdy", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 2, "M", "Grey", "Rowdy", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, "M", "Grey", "Rowdy", true);
